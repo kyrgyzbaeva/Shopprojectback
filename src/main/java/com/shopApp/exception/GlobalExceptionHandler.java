6b87a1b0eddcdb2ee5bc65a5a2d7d6523afb613e
Amina <Kyrgyzbaeva1307@gmail.com>
2024-03-26 13:55:30 +0600

    update exceptionHandler

---
+++ src/main/java/com/shopApp/exception/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.shopApp.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    public ResponseEntity<String> handleException(Exception exception) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Internal server error occurred");
+    }
+}
