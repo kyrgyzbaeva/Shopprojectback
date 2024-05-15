6a835f680b22af23c3674e793de99439221f95e7
kyrgyzbaeva <133115394+kyrgyzbaeva@users.noreply.github.com>
2024-05-16 01:04:55 +0600

    Add files via upload
    add role model

---
+++ src/main/java/com/shopApp/model/Role.java
@@ -0,0 +1,19 @@
+package kg.edu.alatoo.online.shop.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20)
+    private Roles name;
+}
---
+++ src/main/java/com/shopApp/model/Roles.java
@@ -0,0 +1,7 @@
+package kg.edu.alatoo.online.shop.model;
+
+public enum Roles {
+    ROLE_USER,
+    ROLE_SELLER,
+    ROLE_ADMIN
+}
