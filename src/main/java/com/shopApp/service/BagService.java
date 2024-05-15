8f77739e0a7333e7d0d66b81c813693c0857a1eb
kyrgyzbaeva <133115394+kyrgyzbaeva@users.noreply.github.com>
2024-05-16 01:08:05 +0600

    Update BagService.java

--- src/main/java/com/shopApp/service/BagService.java
+++ src/main/java/com/shopApp/service/BagService.java
@@ -1,49 +1,17 @@
-package com.shopApp.service;
+package kg.edu.alatoo.online.shop.service;
 
-import com.shopApp.DTO.response.BagDTO;
-import com.shopApp.DTO.mapper.BagMapper;
-import com.shopApp.entity.Bag;
-import com.shopApp.repository.BagRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import kg.edu.alatoo.online.shop.model.User;
+import kg.edu.alatoo.online.shop.dto.BagRequestDTO;
+import kg.edu.alatoo.online.shop.dto.BagResponseDTO;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Service
-public class BagService {
-
-    private final BagRepository bagRepository;
-    private final BagMapper bagMapper;
-
-    @Autowired
-    public BagService(BagRepository bagRepository, BagMapper bagMapper) {
-        this.bagRepository = bagRepository;
-        this.bagMapper = bagMapper;
-    }
-
-    public List<BagDTO> getAllBagDTOs() {
-        List<Bag> bags = bagRepository.findAll();
-        return bags.stream().map(bagMapper::bagToBagDTO).collect(Collectors.toList());
-    }
-
-    public Optional<BagDTO> getBagDTOById(Long id) {
-        return bagRepository.findById(id).map(bagMapper::bagToBagDTO);
-    }
-
-    public BagDTO saveBagDTO(BagDTO bagDTO) {
-        Bag bag = bagMapper.bagDTOToBag(bagDTO);
-        return bagMapper.bagToBagDTO(bagRepository.save(bag));
-    }
-
-    public void deleteBagById(Long id) {
-        bagRepository.deleteById(id);
-    }
-
-    public BagDTO updateBagDTO(Long id, BagDTO newBagDTO) {
-        Bag existingBag = bagMapper.bagDTOToBag(newBagDTO);
-        existingBag.setId(id);
-        return bagMapper.bagToBagDTO(bagRepository.save(existingBag));
-    }
+public interface BagService {
+    BagResponseDTO create(BagRequestDTO itemRequestDTO, User user);
+    BagResponseDTO update(Long id, BagRequestDTO itemRequestDTO, User user);
+    BagResponseDTO get(Long id, User user);
+    List<BagResponseDTO> getAll(User user);
+    void delete(Long id);
 }
