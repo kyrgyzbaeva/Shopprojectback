75b1a0a9996195b3283407b118c6a2aa01d4cf8c
kyrgyzbaeva <133115394+kyrgyzbaeva@users.noreply.github.com>
2024-05-16 01:16:21 +0600

    Add files via upload
    add refresh token

---
+++ src/main/java/com/shopApp/security/service/RefreshTokenService.java
@@ -0,0 +1,55 @@
+package kg.edu.alatoo.online.shop.security.service;
+
+import kg.edu.alatoo.online.shop.model.RefreshToken;
+import kg.edu.alatoo.online.shop.exception.RefreshTokenNotFoundException;
+import kg.edu.alatoo.online.shop.repository.RefreshTokenRepository;
+import kg.edu.alatoo.online.shop.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Instant;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class RefreshTokenService {
+    @Value("${security-config.app.jwtRefreshExpirationMs}")
+    private Long refreshTokenDurationMs;
+
+    @Autowired
+    private RefreshTokenRepository refreshTokenRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public Optional<RefreshToken> findByToken(String token) {
+        return refreshTokenRepository.findByToken(token);
+    }
+
+    public RefreshToken createRefreshToken(Long userId) {
+
+        RefreshToken refreshToken = new RefreshToken();
+        refreshToken.setUser(userRepository.findById(userId).get());
+        refreshToken.setExpiryDate(Instant.now().plusMillis(refreshTokenDurationMs));
+        refreshToken.setToken(UUID.randomUUID().toString());
+
+        refreshToken = refreshTokenRepository.save(refreshToken);
+        return refreshToken;
+    }
+
+    public RefreshToken verifyExpiration(RefreshToken token) {
+        if (token.getExpiryDate().compareTo(Instant.now()) < 0) {
+            refreshTokenRepository.delete(token);
+            throw new RefreshTokenNotFoundException(token.getToken(), "Время рабочего сеанса истекло, войдите снова");
+        }
+
+        return token;
+    }
+
+    @Transactional
+    public void deleteByUserId(Long userId) {
+        refreshTokenRepository.deleteByUser(userRepository.findById(userId).get());
+    }
+}
