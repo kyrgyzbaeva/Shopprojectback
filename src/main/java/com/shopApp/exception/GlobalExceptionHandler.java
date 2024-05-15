d6695518f7f21817b1bcdda7ecb4ac197274910b
kyrgyzbaeva <133115394+kyrgyzbaeva@users.noreply.github.com>
2024-05-16 01:09:25 +0600

    Update GlobalExceptionHandler.java

--- src/main/java/com/shopApp/exception/GlobalExceptionHandler.java
+++ src/main/java/com/shopApp/exception/GlobalExceptionHandler.java
@@ -1,17 +1,39 @@
-package com.shopApp.exception;
+package kg.edu.alatoo.online.shop.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(Exception.class)
-    @ResponseBody
-    public ResponseEntity<String> handleException(Exception exception) {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Internal server error occurred");
+    @ExceptionHandler({EmailNotFoundException.class})
+    public ResponseEntity<Object> handleStudentNotFoundException(EmailNotFoundException exception) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(exception.getMessage());
+    }
+
+    @ExceptionHandler({RefreshTokenNotFoundException.class})
+    public ResponseEntity<Object> handleStudentNotFoundException(RefreshTokenNotFoundException exception) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(exception.getMessage());
+    }
+
+    @ExceptionHandler({RoleNotFoundException.class})
+    public ResponseEntity<Object> handleStudentNotFoundException(RoleNotFoundException exception) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(exception.getMessage());
+    }
+
+    @ExceptionHandler({BagNotFoundException.class})
+    public ResponseEntity<Object> handleStudentNotFoundException(BagNotFoundException exception) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(exception.getMessage());
     }
 }
+
