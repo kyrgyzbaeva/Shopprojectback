8ec2c3464907324aa9253eee4d4f1c29ded71560
kyrgyzbaeva <133115394+kyrgyzbaeva@users.noreply.github.com>
2024-05-16 01:05:40 +0600

    Delete src/main/java/com/shopApp/repository/UserRepository.java

--- src/main/java/com/shopApp/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.shopApp.repository;
-
-import com.shopApp.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
-
