6dbff96637df7286f1568f4f6c6b985265e3131c
kyrgyzbaeva <133115394+kyrgyzbaeva@users.noreply.github.com>
2024-05-16 01:08:56 +0600

    Update EmailService.java

--- src/main/java/com/shopApp/service/EmailService.java
+++ src/main/java/com/shopApp/service/EmailService.java
@@ -1,49 +1,20 @@
-package com.shopApp.service;
+package kg.edu.alatoo.online.shop.service.impl;
 
-import com.shopApp.DTO.mapper.UserMapper;
-import com.shopApp.DTO.response.UserDTO;
-import com.shopApp.entity.User;
-import com.shopApp.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
-
 @Service
-public class UserService {
+public class EmailService {
+    private final JavaMailSender javaMailSender;
 
-    private final UserRepository userRepository;
-    private final UserMapper userMapper;
-
-    @Autowired
-    public UserService(UserRepository userRepository, UserMapper userMapper) {
-        this.userRepository = userRepository;
-        this.userMapper = userMapper;
+    public EmailService(JavaMailSender javaMailSender) {
+        this.javaMailSender = javaMailSender;
     }
 
-    public List<UserDTO> getAllUserDTOs() {
-        List<User> users = userRepository.findAll();
-        return users.stream().map(userMapper::toDTO).collect(Collectors.toList());
-    }
-
-    public Optional<UserDTO> getUserDTOById(Long id) {
-        return userRepository.findById(id).map(userMapper::toDTO);
-    }
-
-    public UserDTO saveUserDTO(UserDTO userDTO) {
-        User user = userMapper.toEntity(userDTO);
-        return userMapper.toDTO(userRepository.save(user));
-    }
-
-    public void deleteUserById(Long id) {
-        userRepository.deleteById(id);
-    }
-
-    public UserDTO updateUser(Long id, UserDTO newUserDTO) {
-        User newUser = userMapper.toEntity(newUserDTO);
-        newUser.setId(id);
-        return userMapper.toDTO(userRepository.save(newUser));
+    @Async
+    public void sendEmail(SimpleMailMessage email) {
+        javaMailSender.send(email);
     }
 }
