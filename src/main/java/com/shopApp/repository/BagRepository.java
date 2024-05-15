ff44b01b0b8c1a3ad09ffb9485a4aa257c1b7aca
kyrgyzbaeva <133115394+kyrgyzbaeva@users.noreply.github.com>
2024-05-16 01:06:38 +0600

    Update BagRepository.java

--- src/main/java/com/shopApp/repository/BagRepository.java
+++ src/main/java/com/shopApp/repository/BagRepository.java
@@ -1,8 +1,14 @@
-package com.shopApp.repository;
+package kg.edu.alatoo.online.shop.repository;
 
-import com.shopApp.entity.Bag;
+import kg.edu.alatoo.online.shop.model.Bag;
+import kg.edu.alatoo.online.shop.model.User;
+import kg.edu.alatoo.online.shop.dto.BagResponseDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface BagRepository extends JpaRepository<Bag, Long> {
+    List<BagResponseDTO> findByUser(User user);
 }
-
